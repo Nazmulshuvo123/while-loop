6535cfbba1d77f5a95c6da43716e29f06e0fcab1
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-03 11:12:35 +0600

    make multiplication table

---
+++ multiplication.js
@@ -0,0 +1,12 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+ const num = 5;
+ let n = 1;
+ while(n <= 10){
+    let result = num * n;
+   console.log( num + " x " + n + " = " + result);
+   n++;
+ }
